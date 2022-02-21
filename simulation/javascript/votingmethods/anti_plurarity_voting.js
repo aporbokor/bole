efef6941bf5fcd7ad3f8a736b48a3aa65a7f6574
Apor Bokor <aporbokor@protonmail.com>
2022-02-21 18:39:22 +0100

    changes to landing page

--- simulation/javascript/votingmethods/anti_plurarity_voting.js
+++ simulation/javascript/votingmethods/anti_plurarity_voting.js
@@ -16,18 +16,18 @@ class AntiPlurarityVoter extends NumberVotecountVotingMethod {
   }
 
   registrate_strategic_vote(voter) {
-    let min_distance = Infinity;
-    let min_candidate = seems_win_candidates[0];
+    let max_distance = 0;
+    let max_candidate = seems_win_candidates[0];
 
     for (let i = 0; i < seems_win_candidates.length; i++) {
       let curr_cand = seems_win_candidates[i];
       let distance = voter.distance_to_candidate(curr_cand);
-      if (distance < min_distance) {
-        min_distance = distance;
-        min_candidate = curr_cand;
+      if (distance > max_distance) {
+        max_distance = distance;
+        max_candidate = curr_cand;
       }
-      min_candidate.votes += 1;
-      voter.voted_for = min_candidate;
+      max_candidate.votes += 1;
+      voter.voted_for = max_candidate;
     }
   }
 
